daf7d189fbf4bd594a29a5017c516b9e64715155
Keiichi Takahashi <bitwalk@gmail.com>
2021-04-24 07:53:01 +0900

    update

--- qq_plot_2.py
+++ qq_plot_2.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pingouin as pg
 
-np.random.seed(123)
-x = np.random.normal(size=50)
+x = np.random.normal(size=100)
 ax = pg.qqplot(x, dist='norm')
 plt.show()
