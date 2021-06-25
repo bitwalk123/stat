b68092ceed324466451e80f0b5f1f85bf522f3a8
Fuhito Suguri <bitwalk123@users.noreply.github.com>
2021-06-25 19:45:42 +0900

    update

--- excel_write.py
+++ excel_write.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
+# Reference
+# https://note.nkmk.me/python-pandas-to-excel/
 print(pd.__version__)
 # 1.2.2
 
 df = pd.DataFrame([[11, 21, 31], [12, 22, 32], [31, 32, 33]],
-                  index=['one', 'two', 'three'], columns=['a', 'b', 'c'])
+                  index=['one', 'two', 'three'],
+                  columns=['a', 'b', 'c'])
 
 print(df)
 #         a   b   c
